a09fd2ecca9506797085035a14d47f562d51c75c
El_Hasakeh <mr.rebo@mail.ru>
2024-07-26 11:51:27 +0300

    00

---
+++ Day00/src/ex00/Program.java
@@ -0,0 +1,15 @@
+public class Program
+{
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        int num = 479598;
+
+        while (num > 0)
+        {
+            sum += num % 10;
+            num = num /10;
+        }
+        System.out.println(sum);
+    }
+}
