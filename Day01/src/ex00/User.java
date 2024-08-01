c4bc01bf0fab7813c64bdfd77416ea8ecdfe15d7
El_Hasakeh <mr.rebo@mail.ru>
2024-08-01 15:13:13 +0300

    1.00

---
+++ Day01/src/ex00/User.java
@@ -0,0 +1,13 @@
+public class User
+{
+    int id;
+    String name;
+    int balance;
+
+    User(int id, String name, int balance)
+    {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+}
