bc78664b44a01e230e3b5b0804dd0e96d65a1f81
El_Hasakeh <mr.rebo@mail.ru>
2024-08-04 20:50:50 +0300

    Typ

--- Day01/src/ex00/Transaction.java
+++ Day01/src/ex00/Transaction.java
@@ -2,4 +2,15 @@ public class Transaction
 {
     int id;
     int amount;
+    Typ balance;
+    User Sender;
+    User Recipient;
+
+    Transaction(int id, User Recipient, User Sender, Typ balance, int amount)
+    {
+        if (Recipient.getBalance() > 0 && Sender.getBalance() > 0 &&
+                (balance == Typ.DEBITS && Sender.getBalance -amout >= 0
+                || balance == Typ.CREDITS && Recipient.getBalance() -amount >=0))
+            if (balance == Typ)
+    }
 }
