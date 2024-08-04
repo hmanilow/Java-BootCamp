7300c722ffd446b67c4870a9d0e3ac1ad972b4cb
El_Hasakeh <mr.rebo@mail.ru>
2024-08-04 20:20:01 +0300

    1.00.02

--- Day01/src/ex00/Program.java
+++ Day01/src/ex00/Program.java
@@ -2,6 +2,10 @@ public class Program
 {
     public static void main(String[] args)
     {
+        User hmanilow = new User(0, "hmanilow", 850);
+        User kdakota = new User(1, "kdakota", 210);
+
+
 
     }
 }
