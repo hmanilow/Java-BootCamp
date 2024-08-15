6b1e2777e3346513817c5f2e8f11463257848db8
El_Hasakeh <mr.rebo@mail.ru>
2024-08-15 19:56:43 +0300

    trans

--- Day01/src/ex00/Transaction.java
+++ Day01/src/ex00/Transaction.java
@@ -6,11 +6,25 @@ public class Transaction
     User Sender;
     User Recipient;
 
-    Transaction(int id, User Recipient, User Sender, Typ balance, int amount)
-    {
+    Transaction(int id, User Recipient, User Sender, Typ balance, int amount) {
         if (Recipient.getBalance() > 0 && Sender.getBalance() > 0 &&
-                (balance == Typ.DEBITS && Sender.getBalance -amout >= 0
-                || balance == Typ.CREDITS && Recipient.getBalance() -amount >=0))
-            if (balance == Typ)
+                (balance == Typ.DEBITS && Sender.getBalance - amout >= 0
+                        || balance == Typ.CREDITS && Recipient.getBalance() - amount >= 0)) {
+            if (balance == Typ) {
+                Sender.setBalance(Sender.getBalance() - amount);
+                Recipient.setBalance(Recipient.getBalance() + amount);
+            } else {
+                Sender.setBalance(Sender.getBalance() + amount);
+                Recipient.setBalance(Recipient.getBalance - amount);
+            }
+            this.id = id;
+            this.Recipient = Recipient;
+            this.Sender = Sender;
+            this.balance = balance;
+            this.amount = amount;
+            System.out.println("Transaction completed");
+        }
+        else
+            System.out.println("Wrong balance");
     }
 }
