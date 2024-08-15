a09fc0f60534fbc10ca6f7e35c489005bf7567df
El_Hasakeh <mr.rebo@mail.ru>
2024-08-15 20:02:34 +0300

    prog

--- Day01/src/ex00/Program.java
+++ Day01/src/ex00/Program.java
@@ -5,7 +5,10 @@ public class Program
         User hmanilow = new User(0, "hmanilow", 850);
         User kdakota = new User(1, "kdakota", 210);
 
+        Transaction t0 = new Transaction(0,hmanilow, kdakota, Typ.CREDITS, 50);
 
+        System.out.println(hmanilow.getBalance());
+        System.out.println(kdakota.getBalance());
 
     }
 }
